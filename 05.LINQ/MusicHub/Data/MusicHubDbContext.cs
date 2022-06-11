72e4cd5344c6e2216d579edd744089b868e2e85f
PeterTodorovv <petertodorovv@gmail.com>
2022-06-11 18:59:35 +0300

    06/11

---
+++ 05.LINQ/MusicHub/Data/MusicHubDbContext.cs
@@ -0,0 +1,42 @@
+﻿namespace MusicHub.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MusicHub.Data.Models;
+
+    public class MusicHubDbContext : DbContext
+    {
+        public MusicHubDbContext()
+        {
+        }
+
+        public MusicHubDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Album> Albums { get; set; }
+        public virtual DbSet<Performer> Performers { get; set; }
+        public virtual DbSet<Producer> Producers { get; set; }
+        public virtual DbSet<Song> Songs { get; set; }
+        public virtual DbSet<Writer> Writers { get; set; }
+        public virtual DbSet<SongPerformer> SongsPerformers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<SongPerformer>(sp => sp.HasKey(k => new {k.SongId, k.PerformerId }));
+        }
+    }
+}
