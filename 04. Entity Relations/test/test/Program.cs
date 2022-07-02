4efcce4d270013d6f0a885522e4265317b273ca8
PeterTodorovv <petertodorovv@gmail.com>
2022-07-02 19:01:51 +0300

    07/02

---
+++ 04. Entity Relations/test/test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[]
+            {
+                1, 2 ,3, 4, 4 , 4, 1
+            };
+
+            Console.WriteLine(array.Min() + array.Max());
+        }
+    }
+}
